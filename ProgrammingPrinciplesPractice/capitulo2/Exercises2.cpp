49a963c67dc7d7360b078d4f9eeba7f52ef3a69f
EmilioSoDi <idv18c.esolis@uartesdigitales.edu.mx>
2019-09-13 13:18:23 -0500

    Ejercicios del capitulo 2 No estaban en el commit anterior

---
+++ ProgrammingPrinciplesPractice/capitulo2/Exercises2.cpp
@@ -0,0 +1,74 @@
+#include "Includes.h"
+
+int main()
+{
+	//1. Change the program "hello_world.cpp" to output the two lines
+	cout << "I.\nHello, programming!\nHere we go!\n";
+	cout << endl << endl;
+
+	//2. Instructions to find upstairs bathroom
+	cout << "II. Instructions to find upstairs bathroom\n";
+	cout << endl;
+	cout << "1. Stop current action\n2. Stand up\n3. Turn right\n4. Walk forward until you reach the stairs\n";
+	cout << "5. Go up step by step until you reach the top\n6. Walk 3 steps forward\n7.Turn left\n";
+	cout << "8. Twist doorknob with hand and push simultanously\nCongratulations! You have arrived.\n";
+	cout << endl << endl;
+
+	//3. Instructions from front UAD's frontdoor
+	cout << "III. Instructions from front UAD's frontdoor\n";
+	cout << endl;
+	cout << "1. Get on bycile\n2. Check if the road is clear\n3. Do a circular motion with your legs to advance\n";
+	cout << "4. Turn right\n5. Go forward until you reach Av. Lopez Mateos\n6. Turn right\n7. Go forward staying ";
+	cout << "on the right side of the road until you reach Calle Jose Maria Vigil\n8. Turn right\n9. Go forward ";
+	cout << "until you reach Calle Andres Teran\n10. Turn left\n11. Stick to the right\n12. Go forward until you reach UAD\n";
+	cout << "13. Use the doorbell\n14. Wait\n15. Get in\nCongratulations! You have arrived. ";
+	cout << "NOTE: Respect and follow traffic laws.\n";
+	cout << endl << endl;
+
+	//4. Rewrite a blueberry muffin recipe
+	cout << "IV. Blueberry Muffin Recipe\n";
+	cout << endl;
+	cout << "Tools:\nMeasuring cup, teaspoon, oven, muffin liner or muffin cup, whisks, bowl, mitts\n";
+	cout << endl;
+	cout << "Ingredients:\nAll-purpose flour, white sugar, salt, baking powder, vegetable oil,\negg, milk, fresh blue berries, ";
+	cout << "cubed butter, ground cinnamon, grease.\n";
+	cout << endl;
+	cout << "Steps:\n1. Turn on oven ontil it hits 200°C (also known as preheat)\n2. Grease muffin cups/liners\n";
+	cout << "3. Add 1 1/2 cups flour, 3/4 cup sugar, salt and baking powder to the bowl\n4. Combine the ingredients using the whisk\n";
+	cout << "5. Add 1 cup vegetable oil and 1 egg to the measuring cup, and milk until it is full\n6. Add to the bowl\n";
+	cout << "7. Mix ingredients using the whisks\n8. Add unespecified ammount of blueberries after mixed\n";
+	cout << "9. Fill muffin cups/liners with the mixture found in the bowl\n10. Put on mitts\n11. Open the oven\n";
+	cout << "12. Grab the muffin cups/liners and place them inside the oven carefully\n13. Close the oven\n14. Wait 25 minutes\n";
+	cout << "15. (Put on mitts if not currently on) Turn off the oven\n16. Open the oven\n17. Remove muffin cups/liners from the oven\n";
+	cout << "18. Place muffin cups/liners somewhere\n19. Remove mitts\n20. Wait for the muffins to cool down\n";
+	cout << "21. Remove muffins with muffin luners from the muffin cups, and the muffins are ready to eat (do not eat the liners).\n";
+	cout << "NOTE: You can remove the mitts while waiting between steps 14 and 15\n";
+	cout << endl << endl;
+
+	//5. Self definition of terms in "Terms"
+	cout << "Terms:\n";
+	cout << endl;
+	cout << "// - Se usa para dejar comentarios\n";
+	cout << "executable - Es el resultado final de un programa completo, lo que hace correr a un programa\n";
+	cout << "main() - La funcion principal del programa\n";
+	cout << "<< - Indica que la compu hara un output\n";
+	cout << "function - Bloque que recibe parametros, puede dar un valor de retorno y se puede llamar en otras partes del programa\n";
+	cout << "object code - Es el codigo de un archivo compilado\n";
+	cout << "C++ - Es un lenguaje de programacion\n";
+	cout << "header - Es un tipo de archivo. Pueden ser libraries, contener definiciones\n";
+	cout << "output - Es lo que recibe uno de parte de la computadora. Ejemplo 'Hello world! en la pantalla\n";
+	cout << "comment - Ayudan a hacer mas legible el codigo para el humano\n";
+	cout << "IDE - Integrated/Interactive Development Environment, como un editor de texto pero enfocado a editar codigo\n";
+	cout << "program - object codes listos para ser unidos por el linker\n";
+	cout << "compiler - Es lo que produce object code, hace que el codigo sea posible de correr\n";
+	cout << "#include - Es lo que permite incluir u archivo a otro. Ejemplo: #includes incluye 'Includes.h' a '___.cpp'\n";
+	cout << "source code - Es el codigo en su primer estado, como este .cpp sin compilar ni correr\n";
+	cout << "compile-time error - Errores que el compilador detecta, errores que no permiten compilar y evita que el programa llegue al linker\n";
+	cout << "library - Codigo que contienen declaraciones y sentencias que nos permite usar para otros programas\n";
+	cout << "statement - Una declaracion o instruccion, sin esto no ocurre nada\n";
+	cout << "cout - La manera standard del flujo de output o de output stream\n";
+	cout << "linker - Es lo que une object codes, para poder hacer un executable\n";
+
+	cin.get();
+	return 0;
+}
